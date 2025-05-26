1fa2c572d165d899cddddb357835a9dda0e657e4
Douglas Araújo <douglasoliveiraarjo@gmail.com>
2025-05-26 09:16:50 -0300

    feat: adiciona exercícios práticos em atividades-php

---
+++ atividades-php/atividade-8.php
@@ -0,0 +1,11 @@
+<?php
+
+// Definindo variáveis
+$n1 = 2;
+$n2 = 5;
+$n3 = 8;
+
+$soma = $n1 + $n2 + $n3;
+
+// Exibindo o resultado 
+echo "A soma é: " . $soma;
---
+++ atividades-php/atividade-9.php
@@ -0,0 +1,12 @@
+<?php
+
+
+
+$nota1 = 7.5;
+$nota2 = 8.0;
+$nota3 = 6.5;
+
+
+$media = ($nota1 + $nota2 + $nota3) / 3;
+
+echo "A média das três notas é: ".number_format($media, 2);

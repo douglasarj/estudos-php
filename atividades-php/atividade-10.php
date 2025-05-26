b653a4806141099f8577bc038cfb57c158072132
Douglas Araújo <douglasoliveiraarjo@gmail.com>
2025-05-26 09:35:11 -0300

    feat: adiciona exercícios práticos em atividades-php

---
+++ atividades-php/atividade-10.php
@@ -0,0 +1,15 @@
+<?php
+
+$instrutor = "Rafael";
+
+$n1 = 2;
+$n2 = 3;
+$n3 = 4;
+$n4 = 5;
+$n5 = 6;
+
+$resultado = $n1 * $n2 * $n3 * $n4 * $n5;
+
+echo "Instrutor: " . $instrutor . "<br>";
+echo "Números: $n1, $n2, $n3, $n4, $n5<br>";
+echo "Resultado da multiplicação: " . $resultado;

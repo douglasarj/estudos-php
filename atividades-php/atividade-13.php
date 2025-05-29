5da315dd4dbad15e048c29731270472b0aac035f
Douglas Araújo <douglasoliveiraarjo@gmail.com>
2025-05-29 09:13:11 -0300

    feat: adiciona exercícios práticos em atividades-php

---
+++ atividades-php/atividade-13.php
@@ -0,0 +1,10 @@
+<?php
+
+$idade = 20;
+
+if ($idade >= 16) {
+  echo "Você pode votar!";
+} else {
+  echo "Você ainda não pode votar";
+}
+
---
+++ atividades-php/atividade14.php
@@ -0,0 +1,12 @@
+<?php
+
+$numero = -5; 
+
+if ($numero > 0) {
+  echo "O número $numero é positivo";
+} elseif ($numero < 0) {
+  echo "O número $numero é negativo";
+} else {
+  echo "O número é zero";
+}
+
